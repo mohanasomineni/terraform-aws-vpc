a3e4bb2d93c8f32d1e26c31c4c65fd9809648892
Mohanasomineni <mohanamanikanta7@gmail.com>
2025-02-19 15:39:05 -0500

    terraform

--- data.tf
+++ data.tf
@@ -2,9 +2,6 @@ data "aws_availability_zones" "available" {
     state = "available"
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
 
 data "aws_route_table" "main" {
   vpc_id = local.default_vpc_id
